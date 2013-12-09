c7ebb5ba6921a59b37b6e3f6067a3acffc22c9e4
Bhupati Patel <bpatel@outsideiq.com>
2013-12-09 20:55:10 +0530

    Sample: Commiting test.

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringUtils;
 
 public class Test {
 	public static void main(String[] args) {
+		//This is branch 1 change
 		String url = "https://twitter.com/bhupatipatel009";
 		System.out.println(StringUtils.substring(url, StringUtils.lastIndexOf(url, '/') + 1));
 	}
